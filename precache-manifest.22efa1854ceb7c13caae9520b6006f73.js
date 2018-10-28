a80e6dd83ce08994a41c7cb49604d3558c97a0a2
VaniushaR <vaniusha.ra@gmail.com>
2018-10-28 16:19:08 -0600

    Updates

---
+++ precache-manifest.22efa1854ceb7c13caae9520b6006f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HERE-Hackathon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2cce08390fa836b810d9",
+    "url": "/HERE-Hackathon/static/js/runtime~main.2cce0839.js"
+  },
+  {
+    "revision": "f7621177e94a642453e3",
+    "url": "/HERE-Hackathon/static/js/main.f7621177.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/HERE-Hackathon/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "f7621177e94a642453e3",
+    "url": "/HERE-Hackathon/static/css/main.c6ce76c7.chunk.css"
+  },
+  {
+    "revision": "2f3faaa90928888882ac189ae9f643fe",
+    "url": "/HERE-Hackathon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/VaniushaR/HERE-Hackathon.git/precache-manifest.7b539cfb72a2134daf065820cfcd557c.js"
+  "/HERE-Hackathon/precache-manifest.22efa1854ceb7c13caae9520b6006f73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/VaniushaR/HERE-Hackathon.git/index.html", {
+workbox.routing.registerNavigationRoute("/HERE-Hackathon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
